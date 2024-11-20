001c7df9ca65d74bf0156ddcf9537340b7cbfa8e
womaninwork <womaninworkk@gmail.com>
2024-11-20 19:46:03 -0300

    ajustes

--- app/routes/router.js
+++ app/routes/router.js
@@ -211,36 +211,7 @@ router.get("/home_prof", function (req, res) {
           usuarioController.resetarSenha(req, res);
       });
         
-//banco de dados//
-router.get('/tabelas', async (req, res) => {
-    try {
-        const [results, fields] = await pool.query('SHOW TABLES');
-        res.json(results);
-    } catch (error) {
-        console.error('Erro ao listar as tabelas:', error);
-        res.status(500).send('Erro ao listar as tabelas');
-    }
-  });
-      
-      
-      router.get(
-        "/ativar-conta",
-        verificarUsuAutenticado,
-        async function (req, res) {
-          usuarioController.ativarConta(req, res);
-        }
-      );
-      
-      
-      router.get(
-        "/adm",
-        verificarUsuAutenticado,
-        verificarUsuAutorizado([2, 3], "pages/restrito"),
-        function (req, res) {
-          res.render("pages/adm", req.session.autenticado);
-        }
-      );
-      
+
   
   
   module.exports = router;
